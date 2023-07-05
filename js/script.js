ece679ab0fff19c8281625c2cfa2dfd63643f95f
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-07-05 22:51:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $(document).ready(function() {
+        // Show/hide the "go to top" button based on scroll position
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 40) {
+            $('#topBtn').fadeIn();
+          } else {
+            $('#topBtn').fadeOut();
+          }
+        });
+      
+        // Scroll to the top when the button is clicked
+        $('#topBtn').click(function() {
+          $('html, body').animate({ scrollTop: 0 }, 1000);
+          return false;
+        });
+      });
+
+   
+
+  
+})
